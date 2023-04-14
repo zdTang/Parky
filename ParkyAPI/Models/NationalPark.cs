c3a8d4324008110f2b61e492abba93f5483eac39
Zhendong Tang <44347399+zdTang@users.noreply.github.com>
2023-04-14 14:32:52 -0400

    2-1 Create data model

---
+++ ParkyAPI/Models/NationalPark.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ParkyAPI.Models
+{
+    public class NationalPark
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        public string Name { get; set; }
+        [Required]
+        public string State { get; set; }
+        public DateTime Created { get; set; }
+        public DateTime Established { get; set; }
+    }
+}
