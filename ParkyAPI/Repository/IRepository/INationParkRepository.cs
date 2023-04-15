545a203a831ab29a35992b425a1ef6915116a91e
Zhendong Tang <44347399+zdTang@users.noreply.github.com>
2023-04-14 20:25:42 -0400

    3-1 Create Repository Interface

---
+++ ParkyAPI/Repository/IRepository/INationParkRepository.cs
@@ -0,0 +1,16 @@
+﻿using ParkyAPI.Models;
+
+namespace ParkyAPI.Repository.IRepository
+{
+    public interface INationParkRepository
+    {
+        ICollection<NationalPark> GetNationalParks();
+        NationalPark GetNationalPark(int nationalParkId);
+        bool NationalParkExists(string name);
+        bool NationalParkExists(int id);
+        bool CreateNatinalPark(NationalPark nationalPark);
+        bool UpdateNatinalPark(NationalPark nationalPark);
+        bool DeleteNatinalPark(int id);
+        bool Save();
+    }
+}
