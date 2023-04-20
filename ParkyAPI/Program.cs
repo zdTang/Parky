42b3bc1747fa57247f84ad225c7a41c5c844a917
Zhendong Tang <44347399+zdTang@users.noreply.github.com>
2023-04-20 09:14:15 -0400

    5-2 Adding Swashbuckle to API

--- ParkyAPI/Program.cs
+++ ParkyAPI/Program.cs
@@ -1,4 +1,5 @@
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
 using ParkyAPI.Data;
 using ParkyAPI.ParkyMapper;
 using ParkyAPI.Repository;
@@ -17,7 +18,10 @@ namespace ParkyAPI
             builder.Services.AddControllers();
             // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
             builder.Services.AddEndpointsApiExplorer();
-            builder.Services.AddSwaggerGen();
+            builder.Services.AddSwaggerGen(options =>
+            {
+                options.SwaggerDoc("ParkyOpenAPISpec", new Microsoft.OpenApi.Models.OpenApiInfo { Title = "Parky API", Version = "1" });
+            });// this is added by default. Maybe the new version Asp.net core did this.
             builder.Services.AddDbContext<ApplicationDbContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
             builder.Services.AddScoped<INationalParkRepository, NationalParkRepository>();
             builder.Services.AddAutoMapper(typeof(ParkyMappings));  // add AutoMapper to Service Container
@@ -26,8 +30,8 @@ namespace ParkyAPI
             // Configure the HTTP request pipeline.
             if (app.Environment.IsDevelopment())
             {
-                app.UseSwagger();
-                app.UseSwaggerUI();
+                app.UseSwagger();  // added by Default
+                app.UseSwaggerUI();// added by Default
             }
 
             app.UseHttpsRedirection();
