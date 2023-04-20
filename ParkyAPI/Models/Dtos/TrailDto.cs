bcfdd0e356f98b2a4213fb065ba4ec246bfb8c44
Zhendong Tang <44347399+zdTang@users.noreply.github.com>
2023-04-20 10:51:37 -0400

    6-1 Trail Model and TrailDto
    Notice the difference between those model. especially Foreign key and navigation model. should understand these model are used into different use case. As domain model, it is for creating Database table. As DTO, it is used when Model Binding and Validation

---
+++ ParkyAPI/Models/Dtos/TrailDto.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace ParkyAPI.Models.Dtos
+{
+    public class TrailDto
+    {
+        public int Id { get; set; }
+        [Required]
+        public string Name { get; set; } = "";
+        [Required]
+        public double Distance { get; set; }
+        public DifficultyType Difficulty { get; set; }
+        [Required]
+        public int NationalParkId { get; set; }
+        public NationalParkDto? NationalPark { set; get; }
+    }
+}
