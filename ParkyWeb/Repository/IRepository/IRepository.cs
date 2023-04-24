d6989f18afbf8582d4a1f7b0b45c5aa27b6ac61e
Zhendong Tang <44347399+zdTang@users.noreply.github.com>
2023-04-24 15:59:01 -0400

    8-4 Repository Interface

---
+++ ParkyWeb/Repository/IRepository/IRepository.cs
@@ -0,0 +1,11 @@
+﻿namespace ParkyWeb.Repository.IRepository
+{
+    public interface IRepository<T> where T : class
+    {
+        Task<T> GetAsync(string url, int Id);
+        Task<IEnumerable<T>> GetAllAsync(string url);
+        Task<bool> CreateAsync(string url, T objectToCreate);
+        Task<bool> UpdateAsync(string url, T objectToUpdate);
+        Task<bool> DeleteAsync(string url, int Id);
+    }
+}
