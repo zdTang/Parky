d2aaa1d02f8d1e7811082a200b17977d4b886e39
Zhendong Tang <44347399+zdTang@users.noreply.github.com>
2023-04-24 15:30:08 -0400

    8-2 API Static Details
    Save those Base URL as static value

---
+++ ParkyWeb/SD.cs
@@ -0,0 +1,9 @@
+﻿namespace ParkyWeb
+{
+    public class SD
+    {
+        public static string APIBaseUrl = "https://localhost:44395/";
+        public static string NationalParkAPIPath = APIBaseUrl + "api/v1/nationalparks";
+        public static string TrailAPIPath = APIBaseUrl + "api/v1/trails";
+    }
+}
