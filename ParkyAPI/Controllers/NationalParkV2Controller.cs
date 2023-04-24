d48ac00fdb645f12bc23d8cba63a4c13d1a17ed9
Zhendong Tang <44347399+zdTang@users.noreply.github.com>
2023-04-24 09:33:19 -0400

    7-2 API versioning - prepare and install packages

---
+++ ParkyAPI/Controllers/NationalParkV2Controller.cs
@@ -0,0 +1,40 @@
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using ParkyAPI.Models;
+using ParkyAPI.Models.Dtos;
+using ParkyAPI.Repository.IRepository;
+
+namespace ParkyAPI.Controllers
+{
+    [Route("api/[controller]")] // Here means we use Controller Name as URL, the Action Name will not be considered!!
+    [ApiController]
+    [ProducesResponseType(StatusCodes.Status400BadRequest)]
+    public class NationalParksV2Controller : ControllerBase
+    {
+        private INationalParkRepository _parkRepository;
+        private readonly IMapper _mapper;
+
+        public NationalParksV2Controller(INationalParkRepository parkRepository, IMapper mapper)
+        {
+            _parkRepository = parkRepository;
+            _mapper = mapper;
+        }
+
+
+        [HttpGet]
+        [ProducesResponseType(200, Type = typeof(List<NationalPark>))]
+        public IActionResult GetNationalParks()
+        {
+            var objList = _parkRepository.GetNationalParks();
+            // Never expose Domain model to outside world
+            var objDto = new List<NationalParkDto>();
+            foreach (var obj in objList)
+            {
+                objDto.Add(_mapper.Map<NationalParkDto>(obj));
+            }
+            return Ok(objDto);
+        }
+
+    }
+}
