9974fb9d9d45e2c125ec0faade5b4fee0f16b433
Zhendong Tang <44347399+zdTang@users.noreply.github.com>
2023-04-24 10:37:14 -0400

    7-4 Move Swagger Configure file to a single file

---
+++ ParkyAPI/ConfigureSwaggerOptions.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc.ApiExplorer;
+using Microsoft.Extensions.Options;
+using Swashbuckle.AspNetCore.SwaggerGen;
+
+namespace ParkyAPI
+{
+    public class ConfigureSwaggerOptions : IConfigureOptions<SwaggerGenOptions>
+    {
+        readonly IApiVersionDescriptionProvider _provider;
+        public ConfigureSwaggerOptions(IApiVersionDescriptionProvider provider) => this._provider = provider;
+
+
+        public void Configure(SwaggerGenOptions options)
+        {
+            foreach (var desc in _provider.ApiVersionDescriptions)
+            {
+                options.SwaggerDoc(desc.GroupName, new Microsoft.OpenApi.Models.OpenApiInfo()
+                {
+                    Title = $"Parky API {desc.ApiVersion}",
+                    Version = desc.ApiVersion.ToString()
+                });
+            }
+        }
+    }
+}
--- ParkyAPI/Program.cs
+++ ParkyAPI/Program.cs
@@ -1,9 +1,12 @@
+using Microsoft.AspNetCore.Mvc.ApiExplorer;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Options;
 using ParkyAPI.Data;
 using ParkyAPI.ParkyMapper;
 using ParkyAPI.Repository;
 using ParkyAPI.Repository.IRepository;
+using Swashbuckle.AspNetCore.SwaggerGen;
 
 namespace ParkyAPI
 {
@@ -18,27 +21,29 @@ namespace ParkyAPI
             builder.Services.AddControllers();
             // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
             builder.Services.AddEndpointsApiExplorer();
-            builder.Services.AddSwaggerGen(options =>
-            {
-                options.SwaggerDoc("ParkyOpenAPISpec", new Microsoft.OpenApi.Models.OpenApiInfo()
-                {
-                    Title = "Parky API",
-                    Version = "1",
-                    Description = "Uemy Parky API",
-                    Contact = new Microsoft.OpenApi.Models.OpenApiContact()
-                    {
-                        Email = "zhendongtt@gmail.com",
-                        Name = "Mike Tang",
-                        Url = new Uri("https://www.Prosites.com")
-                    },
-                    License = new Microsoft.OpenApi.Models.OpenApiLicense()
-                    {
-                        Name = "MIT License",
-                        Url = new Uri("https://en.wikipedia.org/wike/MIT_License")
-                    }
+            builder.Services.AddTransient<IConfigureOptions<SwaggerGenOptions>, ConfigureSwaggerOptions>();
+            builder.Services.AddSwaggerGen();
+            //builder.Services.AddSwaggerGen(options =>
+            //{
+            //    options.SwaggerDoc("ParkyOpenAPISpec", new Microsoft.OpenApi.Models.OpenApiInfo()
+            //    {
+            //        Title = "Parky API",
+            //        Version = "1",
+            //        Description = "Uemy Parky API",
+            //        Contact = new Microsoft.OpenApi.Models.OpenApiContact()
+            //        {
+            //            Email = "zhendongtt@gmail.com",
+            //            Name = "Mike Tang",
+            //            Url = new Uri("https://www.Prosites.com")
+            //        },
+            //        License = new Microsoft.OpenApi.Models.OpenApiLicense()
+            //        {
+            //            Name = "MIT License",
+            //            Url = new Uri("https://en.wikipedia.org/wike/MIT_License")
+            //        }
 
-                });
-            });// this is added by default. Maybe the new version Asp.net core did this.
+            //    });
+            //});// this is added by default. Maybe the new version Asp.net core did this.
             builder.Services.AddDbContext<ApplicationDbContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
             builder.Services.AddScoped<INationalParkRepository, NationalParkRepository>();
             builder.Services.AddScoped<ITrailRepository, TrailRepository>();
@@ -49,18 +54,30 @@ namespace ParkyAPI
                 options.DefaultApiVersion = new Microsoft.AspNetCore.Mvc.ApiVersion(1, 0);
                 options.ReportApiVersions = true;
             });
+            builder.Services.AddVersionedApiExplorer(options => options.GroupNameFormat = "'v'VVV");
+
             var app = builder.Build();
 
             // Configure the HTTP request pipeline.
+            //var apiVersionDescriptionProvider = builder.Services.BuildServiceProvider().GetService<IApiVersionDescriptionProvider>();
+            var provider = app.Services.GetRequiredService<IApiVersionDescriptionProvider>();
             if (app.Environment.IsDevelopment())
             {
                 app.UseSwagger();  // added by Default
                 app.UseSwaggerUI(options =>
                 {
-                    options.SwaggerEndpoint("/swagger/ParkyOpenAPISpec/swagger.json", "Parky API");
-                    //options.RoutePrefix = "";
+                    foreach (var desc in provider.ApiVersionDescriptions)
+                    {
+                        options.SwaggerEndpoint($"/swagger/{desc.GroupName}/swagger.json", desc.GroupName.ToUpperInvariant());
+                        options.RoutePrefix = "";
+                    }
+                });
+                //app.UseSwaggerUI(options =>
+                //{
+                //    options.SwaggerEndpoint("/swagger/ParkyOpenAPISpec/swagger.json", "Parky API");
+                //    //options.RoutePrefix = "";
 
-                });// added by Default
+                //});// added by Default
             }
 
             app.UseHttpsRedirection();
