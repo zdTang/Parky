ece0331bb4bc2150f42ece6fe708e0f92bf404ba
Zhendong Tang <44347399+zdTang@users.noreply.github.com>
2023-04-28 17:12:13 -0400

    9-8 Upsert Post Action

--- ParkyAPI/Models/Dtos/NationalParkDto.cs
+++ ParkyAPI/Models/Dtos/NationalParkDto.cs
@@ -11,5 +11,6 @@ namespace ParkyAPI.Models.Dtos
         public string State { get; set; } = "";
         public DateTime Created { get; set; }
         public DateTime Established { get; set; }
+        public byte[]? Picture { get; set; }
     }
 }
--- ParkyWeb/Controllers/NationalParksController.cs
+++ ParkyWeb/Controllers/NationalParksController.cs
@@ -36,6 +36,46 @@ namespace ParkyWeb.Controllers
             if (obj == null) return NotFound();
             return View(obj);
         }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Upsert(NationalPark obj)
+        {
+            if (ModelState.IsValid)
+            {
+                var files = HttpContext.Request.Form.Files;
+                if (files.Count > 0)
+                {
+                    byte[] p1 = null;
+                    using (var fs1 = files[0].OpenReadStream())
+                    {
+                        using (var ms1 = new MemoryStream())
+                        {
+                            fs1.CopyTo(ms1);
+                            p1 = ms1.ToArray();
+                        }
+                    }
+                    obj.Picture = p1;
+                }
+                else
+                {
+                    var objFromDb = await _parkRepository.GetAsync(SD.NationalParkAPIPath, obj.Id);
+                    obj.Picture = objFromDb?.Picture;
+                }
+                if (obj.Id == 0)
+                {
+                    await _parkRepository.CreateAsync(SD.NationalParkAPIPath, obj);
+                }
+                else
+                {
+                    await _parkRepository.UpdateAsync(SD.NationalParkAPIPath + obj.Id, obj);
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            else
+            {
+                return View(obj);
+            }
+        }
 
         public async Task<IActionResult> GetAllNationalPark()
         {
