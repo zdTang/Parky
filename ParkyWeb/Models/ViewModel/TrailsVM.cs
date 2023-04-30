a5f7d659a57ca7ae48cd649f938e5351460cc2b4
Zhendong Tang <44347399+zdTang@users.noreply.github.com>
2023-04-30 09:07:14 -0400

    10-1 Trails View Model

---
+++ ParkyWeb/Models/ViewModel/TrailsVM.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+
+namespace ParkyWeb.Models.ViewModel
+{
+    public class TrailsVM
+    {
+        public IEnumerable<SelectListItem>? NationalParkList { get; set; }
+        public Trail? Trail { get; set; }
+    }
+}
