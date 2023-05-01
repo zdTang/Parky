5e0d1a543bf61055c01788a53dd4a043a655ea8f
Zhendong Tang <44347399+zdTang@users.noreply.github.com>
2023-05-01 09:46:43 -0400

    11-2 Index View Model and Home Controller

--- ParkyWeb/Controllers/HomeController.cs
+++ ParkyWeb/Controllers/HomeController.cs
@@ -1,5 +1,8 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using ParkyWeb.Models;
+using ParkyWeb.Models.ViewModel;
+using ParkyWeb.Repository;
+using ParkyWeb.Repository.IRepository;
 using System.Diagnostics;
 
 namespace ParkyWeb.Controllers
@@ -7,15 +10,37 @@ namespace ParkyWeb.Controllers
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+        private readonly INationalParkRepository _nationalParkRepository;
+        private readonly ITrailRepository _trailRepository;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(ILogger<HomeController> logger, ITrailRepository trailRepository, INationalParkRepository nationalParkRepository)
         {
             _logger = logger;
+            _trailRepository = trailRepository;
+            _nationalParkRepository = nationalParkRepository;
         }
 
-        public IActionResult Index()
+        //public IActionResult Index()
+        //{
+        //    var trail = _trailRepository.GetAllAsync(SD.TrailAPIPath);
+        //    var nationalPark = _nationalParkRepository.GetAllAsync(SD.NationalParkAPIPath);
+        //    IndexVM listOfParkAndTrails = new IndexVM
+        //    {
+        //        NationalPark = nationalPark.Result,
+        //        Trails = trail.Result,
+        //    };
+        //    return View(listOfParkAndTrails);
+        //}
+
+        // Tutor's approach
+        public async Task<IActionResult> Index()
         {
-            return View();
+            var listOfParkAndTrails = new IndexVM
+            {
+                NationalPark = await _nationalParkRepository.GetAllAsync(SD.NationalParkAPIPath),
+                Trails = await _trailRepository.GetAllAsync(SD.TrailAPIPath),
+            };
+            return View(listOfParkAndTrails);
         }
 
         public IActionResult Privacy()
---
+++ ParkyWeb/Models/ViewModel/IndexVM.cs
@@ -0,0 +1,8 @@
+﻿namespace ParkyWeb.Models.ViewModel
+{
+    public class IndexVM
+    {
+        public IEnumerable<NationalPark>? NationalPark { get; set; }
+        public IEnumerable<Trail>? Trails { get; set; }
+    }
+}
