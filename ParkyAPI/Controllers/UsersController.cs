aa0d9810ea86bd5c8b3cc7717f4aad71ef772e7a
Zhendong Tang <44347399+zdTang@users.noreply.github.com>
2023-05-06 12:50:49 -0400

    12.8 Add Authenticate Method

---
+++ ParkyAPI/Controllers/UsersController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using ParkyAPI.Models;
+using ParkyAPI.Repository.IRepository;
+
+namespace ParkyAPI.Controllers
+{
+    [Route("api/v{version:apiVersion}/users")]
+    [ApiController]
+    [Authorize]
+    public class UsersController : ControllerBase
+    {
+        private readonly IUserRepository _userRepository;
+        public UsersController(IUserRepository userRepository)
+        {
+            _userRepository = userRepository;
+        }
+
+
+
+
+        [HttpPost("authenticate")]
+        public IActionResult Authenticate([FromBody] User model)
+        {
+            var user = _userRepository.Authenticate(model.UserName!, model.Password!);
+            if (user == null)
+                //return BadRequest(ModelState);
+                return BadRequest(new { message = "Username or password is incorrect" });
+            return Ok(user);
+        }
+    }
+}
