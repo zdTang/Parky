5b4e98e86b24abbff8ea8a28ae4da14ad71ebde5
Zhendong Tang <44347399+zdTang@users.noreply.github.com>
2023-05-07 11:38:39 -0400

    13-2 Implement Login and Register in Account Repository

--- ParkyWeb/Repository/AccountRepository.cs
+++ ParkyWeb/Repository/AccountRepository.cs
@@ -1,5 +1,7 @@
-﻿using ParkyWeb.Models;
+﻿using Newtonsoft.Json;
+using ParkyWeb.Models;
 using ParkyWeb.Repository.IRepository;
+using System.Text;
 
 namespace ParkyWeb.Repository
 {
@@ -11,14 +13,48 @@ namespace ParkyWeb.Repository
             _clientFactory = clientFactory;
         }
 
-        public Task<User> LoginAsync(string url, User objToCreate)
+
+        // Be aware, as we are using API to login, we need pass User to API first, that is why we need this, other than
+        // check database, API will check DB, the Web here will not.
+        public async Task<User?> LoginAsync(string url, User objToCreate)
         {
-            throw new NotImplementedException();
+            var request = new HttpRequestMessage(HttpMethod.Post, url);
+            if (objToCreate != null)
+            {
+                request.Content = new StringContent(JsonConvert.SerializeObject(objToCreate), Encoding.UTF8, "application/json");
+            }
+            else
+            {
+                return new User();
+            }
+            var client = _clientFactory.CreateClient();
+            HttpResponseMessage response = await client.SendAsync(request);
+            if (response.StatusCode == System.Net.HttpStatusCode.OK)
+            {
+                var jsonString = await response.Content.ReadAsStringAsync();
+                return JsonConvert.DeserializeObject<User>(jsonString);
+            }
+            else
+            {
+                return new User();
+            }
         }
 
-        public Task<User> RegisterAsync(string url, User objToCreate)
+        public async Task<bool> RegisterAsync(string url, User objToCreate)
         {
-            throw new NotImplementedException();
+            var request = new HttpRequestMessage(HttpMethod.Post, url);
+            if (objToCreate != null)
+            {
+                request.Content = new StringContent(JsonConvert.SerializeObject(objToCreate), Encoding.UTF8, "application/json");
+            }
+            else
+            {
+                return false;
+            }
+            var client = _clientFactory.CreateClient();
+            HttpResponseMessage response = await client.SendAsync(request);
+            return (response.StatusCode == System.Net.HttpStatusCode.OK);
+
         }
     }
 }
--- ParkyWeb/Repository/IRepository/IAccountRepository.cs
+++ ParkyWeb/Repository/IRepository/IAccountRepository.cs
@@ -4,8 +4,8 @@ namespace ParkyWeb.Repository.IRepository
 {
     public interface IAccountRepository : IRepository<User>
     {
-        Task<User> LoginAsync(string url, User objToCreate);
-        Task<User> RegisterAsync(string url, User objToCreate);
+        Task<User?> LoginAsync(string url, User objToCreate);
+        Task<bool> RegisterAsync(string url, User objToCreate);
 
     }
 }
