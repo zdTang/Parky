3989b9b6fad72f920188ad8a065929fd0bfa1596
Zhendong Tang <44347399+zdTang@users.noreply.github.com>
2023-05-06 21:16:22 -0400

    12-10 Add Bearer to Swigger UI

--- ParkyAPI/ConfigureSwaggerOptions.cs
+++ ParkyAPI/ConfigureSwaggerOptions.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Mvc.ApiExplorer;
 using Microsoft.Extensions.Options;
+using Microsoft.OpenApi.Models;
 using Swashbuckle.AspNetCore.SwaggerGen;
 using System.Reflection;
 
@@ -21,6 +22,38 @@ namespace ParkyAPI
                     Version = desc.ApiVersion.ToString()
                 });
             }
+
+            // These stuff can reference Swigger's documentation
+            options.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+            {
+                Description =
+               "JWT Authorization header using the Bearer scheme. \r\n\r\n " +
+               "Enter 'Bearer' [space] and then your token in the text input below.\r\n\r\n" +
+               "Example: \"Bearer 12345abcdef\"",
+                Name = "Authorization",
+                In = ParameterLocation.Header,
+                Type = SecuritySchemeType.ApiKey,
+                Scheme = "Bearer"
+            });
+
+            options.AddSecurityRequirement(new OpenApiSecurityRequirement()
+                {
+                    {
+                        new OpenApiSecurityScheme
+                        {
+                            Reference = new OpenApiReference
+                            {
+                                Type = ReferenceType.SecurityScheme,
+                                Id = "Bearer"
+                            },
+                            Scheme = "oauth2",
+                            Name = "Bearer",
+                            In = ParameterLocation.Header,
+
+                        },
+                        new List<string>()
+                    }
+                });
             // The following is of API documentation with XML file, Ignore them at this point.
             // As the approach this tutorial provided couple with Swigger
 
--- ParkyAPI/Program.cs
+++ ParkyAPI/Program.cs
@@ -20,7 +20,7 @@ namespace ParkyAPI
             var builder = WebApplication.CreateBuilder(args);
 
             // Add services to the container.
-
+            builder.Services.AddCors();
             builder.Services.AddControllers();
             // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
             builder.Services.AddEndpointsApiExplorer();
