afbbe6b7cf2c3ae28f60ab977bed8f0632430217
Zhendong Tang <44347399+zdTang@users.noreply.github.com>
2023-05-06 22:15:56 -0400

    12-15 Authentication Model

---
+++ ParkyAPI/Models/AuthenticationModel.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ParkyAPI.Models
+{
+    public class AuthenticationModel
+    {
+        [Required]
+        public string UserName { get; set; } = "";
+        [Required]
+        public string Password { get; set; } = "";
+    }
+}
